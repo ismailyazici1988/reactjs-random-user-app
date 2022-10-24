c31a3e695e5d3146bb8dec41e25eb9d61a55aa9f
İsmail Yazıcı <i.yazicigircin18@gmail.com>
2022-10-24 07:56:16 +0300

    add data objects in card

--- src/components/UserCards.jsx
+++ src/components/UserCards.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Figure from "react-bootstrap/Figure";
+import {GrLocationPin} from "react-icons/gr";
 
-const UserCards = () => {
+const UserCards = ({name,location,email,phone,picture,dob}) => {
   return (
-    <div>UserCards</div>
+    <div>
+    <Figure className='d-flex gap-4'>
+      <Figure.Image
+        className='rounded-circle'
+        alt="171x180"
+        src={picture?.large}
+      />
+      <Figure.Caption>
+        <h1>{name?.title} {name?.first} {name?.last}</h1>
+        <h5>{phone}</h5>
+        <h5>{email}</h5>
+        <h5>{new Date(dob?.date).toLocaleDateString("tr-TR")}</h5>
+        <p><GrLocationPin/>{location?.city}</p>
+      </Figure.Caption>
+    </Figure>
+    </div>
   )
 }
 
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,21 +1,27 @@
-import React from "react";
+import {useState,useEffect} from "react";
 import UserCards from "./UserCards";
 
 const Users = () => {
+
+    const [user, setUser] = useState("")
     //js
     const randomUser = () => {
-        fetch("https://randomuser.me/api/").then((response)=> response.json()).then((data)=> console.log(data.results[0]))
+        fetch("https://randomuser.me/api/").then((response)=> response.json()).then((data)=> setUser(data.results[0]))
     }
 
+    useEffect(() => {
+      randomUser()
+    }, [])
+    
+// console.log(user)
 
-
-
+const {name,location,email,phone,picture,dob} = user;
 
   return (
     <div className="my-5">
       <h1>Users</h1>
       <button className="btn btn-dark" onClick={randomUser}>Random User</button>
-      <UserCards />
+      <UserCards name={name} location={location} email={email} phone={phone} picture={picture} dob={dob}/>
     </div>
   );
 };
